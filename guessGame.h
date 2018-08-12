3376df385543b177e2beea731037a5b7289bce1b
AlfredHuang <765123735@qq.com>
2018-08-12 09:56:37 +0800

    添加猜字游戏

---
+++ guessGame.h
@@ -0,0 +1,9 @@
+#ifndef guessGame_h 
+#define guessGame_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void guess();
+#endif
--- main.c
+++ main.c
@@ -1,66 +1,13 @@
 #include <stdio.h>
-//#include <stdlib.h>
-//#include "hello.h"
-//#include "change.h"
-//#include "reverseFunction.h"
-//#include "inputText.h"
-//#include "GPSValidate.h"
-//#include "heightConversion.h"
-//#include "TimeConversion.h"
-//#include "change.h"
-//#include "reverseFunction.h"
-//# include "CompareTwoNumber.h"
-#include "TimeConvert.h"
-#include "rsConvert.h"
+#include <stdlib.h>
+#include <time.h>
+#include "guessGame.h"
+
 
 int main(int argc, char *argv[]) {
 	
-	//findingChange();
-	//heightConvert();
-	//timeConvert();
-	//reverseNumber();
-	//int a=5;
-	//int b=0;
-	//int c = (b=a+2) - (a=2);
-	//printf("%d\n",c);
-	//getMaxInTwoNumber() ;
-//	int n = 5992;
-//	if ( 1<=n<=10 ){
-//		printf("Ok");
-//	}
-	//232
-	///*232*/
-	
-//	int score = 0;
-//	printf("请输入你的分数(0-100):");
-//	scanf("%d", &score);
-//	
-//	if(score < 0 || score > 100) {
-//		printf("输入的分数有误，请输入0至100以内的分数");
-//	} else {	
-//		score = score / 10;		
-//		switch(score){
-//			case 10:
-//			case 9:
-//				printf("A");
-//				break;
-//			case 8:
-//				printf("B");
-//				break;
-//			case 7:
-//				printf("C");
-//				break;
-//			case 6:
-//				printf("D");
-//				break;
-//			default:
-//				printf("E");
-//				break;
-//		}			
-//	} 
-	
-	
-	convertRSInfo(); 
+			
+ 	guess();	
 		
 	return 0;
 }
